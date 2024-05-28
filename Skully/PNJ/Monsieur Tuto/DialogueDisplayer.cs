a0ad690a51611743c226b6046eda6f79bbcc94cc
Philia's Snake <snakephilias@gmail.com>
2024-05-28 09:22:15 +0200

    Rajout des scripts du Tuto

---
+++ Skully/PNJ/Monsieur Tuto/DialogueDisplayer.cs
@@ -0,0 +1,9 @@
+using TMPro;
+using UnityEngine;
+
+public class DialogueDisplayer : MonoBehaviour
+{
+    [SerializeField] private GameObject dialogueBox;
+    [SerializeField] private TextMeshProUGUI dialogueText;
+    public DialogueObject currentDialogue;
+}
---
+++ Skully/PNJ/Monsieur Tuto/DialogueObject.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Dialogue/DialogueObject")]
+public class DialogueObject : ScriptableObject
+{
+    public DialogueLine[] dialogueLines;
+}
