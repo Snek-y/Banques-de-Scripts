8e4a1be28a9f8ed16a1784a2c63b8a8305f48796
Philia's Snake <snakephilias@gmail.com>
2024-10-04 00:20:53 +0200

    Update Program.cs

--- Jeu_du_chameau/Jeu_du_chameau/Program.cs
+++ Jeu_du_chameau/Jeu_du_chameau/Program.cs
@@ -40,16 +40,20 @@
 
 
 
+
+
+
 /// Itération GD du jeu du chameau :
 /// Le joueur est conducteur d'un train à vapeur et il devra gérer le moteur de sa locomotive et son énergie pour remplir le moteur en charbon et en eau.
-
-
-/// Ressource à gérer :
-///          - énergie pour le joueur. 
+/// Le train peut avancer à vitesse normal cela consomme un charbon et un d'eau.
+/// Le train peut avancer à vitesse rapide cela consomme deux charbons et deux d'eau.
+/// La nourriture du joueur sert durant la journée pour le contrôle le train.
+/// à la fin de chaque tour le train récupère un peu de charbon et un peu d'eau pour continuer le voyage.
+/// Gestion de l'état :
+///          - nourriture pour le joueur. 
 ///          - eau de refroidissement moteur du train.
 ///          - Charbon pour faire avancer le train.
-
-
+///          - La distance restante à parcourir.
 /// événement aléatoire :
 ///          - Panne de charbon
 ///          - fuite d'eau
@@ -57,19 +61,147 @@
 ///          - Tempête de neige
 ///          - Surchauffe moteur (lier à l'événement fuite d'eau)
 ///          - Explosion du moteur (lier à l'événement fuite d'eau)
-
 namespace Jeu_du_chameau
 {
     internal class Program
     {
         // pour garder en mémoire le nombre d'eau, de nourriture et de fatigue des personnages
-        int eau = 0;
-        int nourriture = 0;
-        int fatigue = 0;
+        static int eau = 10;
+        static int charbon = 10;
+        static int nourriture = 20;
+        static int distance = 250;
 
-        static void Main()
+        public static void Main()
+        {
+            Debut();
+            Command();
+        }
+
+        public static void Debut()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.WriteLine("Tu as le choix entre 4 commandes : ");
+            Console.WriteLine(" a : La première consiste à faire avancer ton train à la vitesse normal.");
+            Console.WriteLine(" b : La seconde consiste à faire avancer ton train à plus grande vitesse.");
+            Console.WriteLine(" c : La troisième te permet de te reposer et de restaurer tes réserves d'eau, de charbon et de nourriture.");
+            Console.WriteLine(" d : La dernière qui te permet d'afficher le gestionnaire d'état.");
+            Console.WriteLine("Que fais-tu ?");
+        }
+
+        public static void Command()
+        {
+            switch (Console.ReadLine())
+            {
+                case "a":
+                    Console.WriteLine("tu avance de 1");
+                    Suite1();
+                    break;
+
+                case "b":
+                    Console.WriteLine("Tu avance de 2");
+                    Suite2();
+                    break;
+
+                case "c":
+                    Console.WriteLine("tu te repose et tu recharge les réserves de ton train");
+                    Matin();
+                    break;
+
+                case "d": 
+                    Console.WriteLine("Gestionnaire de l'UI");
+                    UI();
+                    break;
+            }
+        }
+
+        public static void UI()
+        {
+            Console.WriteLine("Eau : reste " + eau + " d'eau");
+            Console.WriteLine("Charbon : reste " + charbon + " de charbon");
+            Console.WriteLine("Nourriture : reste " + nourriture + " de nourriture");
+            Console.WriteLine("Distance : reste " + distance + " km de rail");
+            Command();
+        }
+
+        public static void Suite1()
+        {
+            eau -= 1;
+            charbon -= 1;
+
+            if (eau > 0)
+            {
+                Command();
+            }
+            else if (eau == 0)
+            {
+                Die();
+            }
+        }
+
+        public static void Suite2()
+        {
+            eau -= 2;
+            charbon -= 2;
+
+            if (eau > 0)
+            {
+                Command();
+            }
+            else if (eau == 0)
+            {
+                Die();
+            }
+        }
+
+        public static void Matin()
+        {
+            nourriture -= 2;
+            eau +=1;
+            charbon += 1;
+
+            if (nourriture > 0)
+            {
+                Command();
+            }
+            else if (nourriture == 0)
+            {
+                Die();
+            }
+        }
+
+        public static void Evenment()
         {
 
+        }
+
+        public static void Die()
+        {
+            Console.WriteLine("Tu as perdue");
+            Console.WriteLine("Veux-tu recommencer ?");
+            Console.WriteLine(" y : yes.");
+            Console.WriteLine(" n : no.");
+            Retry();
+        }
+
+        public static void Retry()
+        {
+            switch (Console.ReadLine())
+            {
+                case "y":
+                    eau = 10;
+                    charbon = 10;
+                    nourriture = 20;
+                    distance = 250;
+                    Main();
+                    break;
+
+                case "n":
+                    break;
+            }
         }
     }
 }
