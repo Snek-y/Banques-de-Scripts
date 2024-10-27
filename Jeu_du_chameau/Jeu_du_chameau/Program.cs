75461d212e234d6bf02852c236b08bc91b733a41
Philia's Snake <snakephilias@gmail.com>
2024-10-27 10:28:39 +0100

    Update Program.cs

--- Jeu_du_chameau/Jeu_du_chameau/Program.cs
+++ Jeu_du_chameau/Jeu_du_chameau/Program.cs
@@ -29,7 +29,7 @@
         {
             try
             {
-                string cheminFichier = @"C:\Users\rebel\Documents\GitHub\Banques-de-Scripts\Jeu_du_chameau\Jeu_du_chameau\The-Legend-of-Zelda-Spirit-Tracks-Music-Realm-Overworld.wav";
+                string cheminFichier = @"C:\Users\phili\Documents\GitHub\Banques-de-Scripts\Jeu_du_chameau\Jeu_du_chameau\The-Legend-of-Zelda-Spirit-Tracks-Music-Realm-Overworld.wav";
                 if (!System.IO.File.Exists(cheminFichier))
                 {
                     Console.WriteLine("Le fichier audio est introuvable !");
